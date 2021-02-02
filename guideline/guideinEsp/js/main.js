b8b2d56e7662dba68cfd01e23e58ff611c4cc3db
Itaccho <75328597+Itaccho@users.noreply.github.com>
2021-02-02 20:13:11 +0900

    Add files via upload

---
+++ guideline/guideinEsp/js/main.js
@@ -0,0 +1,7 @@
+'use strict'; {
+    const fmac = document.fm.acuerdo;
+    fmac.onclick = function() {
+        document.getElementById('fm').style.display = 'none';
+        document.getElementById('afterAcuerdo').style.display = 'block';
+    }
+}
