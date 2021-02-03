795dcb0794b97eda9b2908649bb9c40b0a13c2e9
Itaccho <75328597+Itaccho@users.noreply.github.com>
2021-02-03 18:33:01 +0900

    Delete Project-JPN/site-chinese/homepage directory

--- Project-JPN/site-chinese/homepage/homepage/forBeginners/js/script.js
+++
@@ -1,8 +0,0 @@
-var clickBtn2 = function() {
-    let p2 = document.getElementById('p2');
-    if (p2.style.display == 'none') {
-        p2.style.display = 'block';
-    } else {
-        p2.style.display = 'none';
-    }
-};
