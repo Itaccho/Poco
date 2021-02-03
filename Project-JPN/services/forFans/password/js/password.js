da2be0ee5ce68a85778e6dbcecb2d5aeac4975fb
Itaccho <75328597+Itaccho@users.noreply.github.com>
2021-02-03 13:41:52 +0900

    Add files via upload

---
+++ Project-JPN/services/forFans/password/js/password.js
@@ -0,0 +1,25 @@
+'use strict'; {
+    document.getElementById('submit').onclick = function() {
+        const pass = document.form1.pass;
+        const cont = document.getElementById('cont');
+        
+
+        if (pass.value == 'nextdoor' ||
+            pass.value == 'e8rA1nf4' ||
+            pass.value == '9o42Kf3' ||
+            pass.value == 'Sho926' ||
+            pass.value == '999999') {
+            document.getElementById('afterCorrect').style.display = 'block';
+            document.getElementById('expl').style.display = 'none';
+        } else {
+            cont.innerHTML = 'パスワードが違います';
+            cont.style.backgroundColor = 'red';
+        }
+    }
+}
+
+// if (formPass.pass.value == 'a') {
+    //     document.getElementById('password').style.display = 'none';
+    //     document.getElementById('expl').style.display = 'none';
+    //     document.getElementById('afterCorrect').style.display = 'block';
+    // }
