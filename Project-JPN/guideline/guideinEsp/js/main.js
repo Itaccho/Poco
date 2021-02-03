edca472780563ab0f87c56e487497f3c8d7442d2
Itaccho <75328597+Itaccho@users.noreply.github.com>
2021-02-04 00:02:17 +0900

    Update main.js

--- Project-JPN/guideline/guideinEsp/js/main.js
+++ Project-JPN/guideline/guideinEsp/js/main.js
@@ -1,3 +1,7 @@
-function acuerdo() {
-     
+'use strict'; {
+    const fmac = document.fm.acuerdo;
+    fmac.onclick = function() {
+        document.getElementById('fm').style.display = 'none';
+        document.getElementById('afterAcuerdo').style.display = 'block';
+    }
 }
